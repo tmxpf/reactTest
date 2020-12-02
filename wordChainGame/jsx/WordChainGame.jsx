6b5a6cfd683c25213995e8dd11e981ee3e706444
gyu <tmxpf@naver.com>
2020-12-03 02:38:17 +0900

    WordChainGame for Hooks

--- wordChainGame/jsx/WordChainGame.jsx
+++ wordChainGame/jsx/WordChainGame.jsx
@@ -1,67 +1,47 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordChainGame extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            word: '바밤바',
-            inputVal: '',
-            result: ''
-        };
+const WordChainGame = () => {
+    const [word, setWord] = useState('바밤바');
+    const [inputVal, setInputVal] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
+
+    const onChangeInput = (e) => {
+        setInputVal(e.target.value);
     };
 
-    onChangeInput = (e) => {
-        let val = e.target.value;
-
-        this.setState({
-            inputVal: val
-        });
-    };
-
-    onSubmitForm = (e) => {
+    const onSubmitForm = (e) => {
         e.preventDefault();
 
-        let firstChar = this.state.inputVal.charAt(0);
-        let lastChar = this.state.word.charAt(this.state.word.length - 1);
-        let good = (preveValue) => {
-            return {
-                word: preveValue.inputVal,
-                inputVal: '',
-                result: '딩동댕!'
-            };
+        let firstChar = inputVal.charAt(0);
+        let lastChar = word.charAt(word.length - 1);
+        let good = () => {
+            setWord(inputVal);
+            setInputVal('');
+            setResult('딩동댕!');
         };
-        let bad = (preveValue) => {
-            return {
-                word: preveValue.word,
-                inputVal: '',
-                result: '땡!'
-            };
+        let bad = () => {
+            setWord(word);
+            setInputVal('');
+            setResult('땡!');
         };
 
-        (firstChar === lastChar) ? this.setState(good) : this.setState(bad);
+        (firstChar === lastChar) ? good() : bad();
 
-        this.input.focus();
+        inputRef.current.focus();
     };
 
-    input;
-
-    inputRef = (c) => {
-        this.input = c;
-    };
-
-    render() {
-        return (
-            <>
-                <div>{this.state.word}</div>
-                <form onSubmit={this.onSubmitForm}>
-                    <input type="text" ref={this.inputRef} onChange={this.onChangeInput} value={this.state.inputVal}/>
-                    <button>입력!</button>
-                </form>
-                <div>{this.state.result}</div>
-            </>
-        );
-    };
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input type="text" ref={inputRef} onChange={onChangeInput} value={inputVal}/>
+                <button>입력!</button>
+            </form>
+            <div>{result}</div>
+        </>
+    );
 }
 
 module.exports = WordChainGame;
